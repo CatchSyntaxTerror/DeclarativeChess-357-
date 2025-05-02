f4517df2e2eb201d1c4db39277ae2f90296010ba
valerie <vbarker@unm.edu>
2025-05-01 20:56:29 -0600

    made the FEN parse into rows

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
 module Main where
 
 import Brillo
-import qualified MyLib (someFunc)
+-- import qualified MyLib (someFunc)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
-  MyLib.someFunc
+  --MyLib.someFunc
   display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
