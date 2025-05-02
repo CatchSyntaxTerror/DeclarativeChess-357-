ecdd79a2b1af4f4fc204b1cf19b62c239a45cc4a
SyntaxTerror <yamin@unm.edu>
2025-05-02 08:26:00 -0600

    GUI?

--- app/Main.hs
+++ app/Main.hs
@@ -7,4 +7,19 @@ main :: IO ()
 main = do
   putStrLn "Hello, Haskell!"
   MyLib.someFunc
-  display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
+  display (InWindow "Declarative Chess" (600, 600) (100, 100))
+
+
+
+squareSize :: Float
+squareSize = 50
+
+chessBoard :: Picture
+chessBoard = Pictures
+  [ translate (x * squareSize - 175) (y * squareSize - 175)
+      (Color (tileColor x y) (rectangleSolid squareSize squareSize))
+  | x <- [0..7], y <- [0..7] ]
+  where
+    tileColor x y = if even (x + y) then cream else greenPastel
+    cream = makeColor 1.0 0.98 0.88 1.0
+    greenPastel = makeColor 0.4 0.6 0.5 1.0
