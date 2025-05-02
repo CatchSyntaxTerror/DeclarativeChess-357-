363e5f6e7a9b49d4ba73a12fb24b1832fcd486cb
SyntaxTerror <yamin@unm.edu>
2025-05-02 08:46:58 -0600

    I think it works

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,25 @@
 module Main where
 
 import Brillo
--- import qualified MyLib (someFunc)
+import ChessLogic
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
-<<<<<<< HEAD
+  putStrLn "Game Loading..."
+
   MyLib.someFunc
   display (InWindow "Declarative Chess" (600, 600) (100, 100))
 
 
 
-squareSize :: Float
+squareSize :: float
 squareSize = 50
 
 chessBoard :: Picture
 chessBoard = Pictures
-  [ translate (x * squareSize - 175) (y * squareSize - 175)
-      (Color (tileColor x y) (rectangleSolid squareSize squareSize))
-  | x <- [0..7], y <- [0..7] ]
+  [translate (x * squareSize - 175) (y * squareSize - 175) 
+             (Color (tileColor x y) (rectangleSolid squareSize squareSize)) | x <- [0..7], y <- [0..7] ]
   where
     tileColor x y = if even (x + y) then cream else greenPastel
     cream = makeColor 1.0 0.98 0.88 1.0
     greenPastel = makeColor 0.4 0.6 0.5 1.0
-=======
-  --MyLib.someFunc
-  display (InWindow "Nice Window" (200, 200) (10, 10)) white (Circle 80)
->>>>>>> 853188c394f71393a67ce926be5411cdaa38d6d4
